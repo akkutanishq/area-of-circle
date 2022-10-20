943f9b09f75fd878faa501b0f6ee14417a2805d7
akkutanishq <86640369+akkutanishq@users.noreply.github.com>
2022-10-20 21:10:18 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,5 @@
+# area of the Circle
+pi = float(3.141592653589793238)
+r=float(input("Input the radius of the circle : \n"))
+area=float(pi*r*r)
+print("The area of the circle with radius 1.1 : ",area)
